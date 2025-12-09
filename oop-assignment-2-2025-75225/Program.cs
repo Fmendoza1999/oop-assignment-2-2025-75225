d3c819808665429a0ff803eed5df9caf791fc05a
Felix Mendoza <felixmr1999@gmail.com>
2025-12-09 10:48:08 +0000

    Add project files.

---
+++ oop-assignment-2-2025-75225/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exam.Questions;
+using oop_assignment_2_2025_75225.Questions;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExamQuestion_1.Run();
+            ExamQuestion_2.Run();
+            ExamQuestion_3.Run();
+
+            // You choose ONE of these – you said Question 5:
+            // ExamQuestion_4.Run();
+            ExamQuestion_5.Run();
+        }
+    }
+}
---
+++ oop-assignment-2-2025-75225/Questions/ExamQuestion_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_assignment_2_2025_75225.Questions
+{
+    internal class ExamQuestion_2
+    {
+    }
+}
